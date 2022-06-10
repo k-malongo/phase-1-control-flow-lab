546472ba75cb71eeca6f2d8ecd64ca1e6c9747c7
Kelvin <vokemalosh32@gmail.com>
2022-06-10 10:01:02 +0300

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(someValue) {
   // Write your code here!
   if (someValue <= 400) {
     return "This one is on me!";
-  } else if (someValue > 2000 && someValue < 2500) {
+  } else if (someValue < 2000 && someValue < 2500) {
     return "I will gladly take your thirty bucks.";
   } else {
     return("No can do.")
